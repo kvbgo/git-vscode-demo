75ddedaae34ca9d6faaebd0ae04bd34cd4b6c66e
Kurt VERBURGH <kurt.verburgh@gmail.com>
2020-11-11 12:30:48 +0100

    clear

---
+++ test.py
@@ -0,0 +1,7 @@
+print("hello world")
+number1=float(input("nummer 1: "))
+number2=float(input("nummer 2: "))
+print("gemiddelde is:",round((number1+number2)/2,4))
+tekst="oostende"
+print("tweede letter is",tekst[1])
+      
